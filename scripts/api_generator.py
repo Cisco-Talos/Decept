60b1380bec859af259b339457fd9e95de10ed910
lilith Wyatt <lil@thiefy.one>
2018-07-06 14:00:06 -0500

    Rough draft of api replaying via --dumpraw output.

---
+++ scripts/api_generator.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+import re
+import os
+import sys
+import md5
+import struct
+# 
+#
+def main(dumpraw_dir,delim="",indexes=""):
+    dir_list = []
+    file_list = []
+    hash_dict = {}
+    request_dict = {}
+
+    if delim:
+        delim_buf = ""
+        if "\\x" in delim: 
+            delim_buf = ''.join([chr(int(x,16)) for x in filter(None,delim.split("\\x"))])
+        else:
+            try:
+                delim_buf = ''.join([chr(int(x,16)) for x in filter(None,delim.split("x"))])
+            except:
+                delim_buf = delim
+
+    # directory structure of a --dumpraw:
+    # basedir
+    #### <dateGenerated>
+    #########session-X-Y-[inbound|outbound]
+    # for x => session num, y => message num in session
+    outdir = os.path.join(dumpraw_dir,"minimized")
+    attempt_counter = 0
+
+    while True:
+        try:
+            os.mkdir(outdir) 
+            print "[0.0] Minimized dir: %s"%outdir
+            break
+        except:
+            outdir = os.path.join(dumpraw_dir,"minimized_%d"%attempt_counter)
+            attempt_counter += 1
+            if attempt_counter >=20:
+                print "Comon, get rid of some of those old minimized_* dirs. Exiting"
+                sys.exit()
+                
+    for f in os.listdir(dumpraw_dir):
+        dirpath = os.path.join(dumpraw_dir,f)
+        if not os.path.isdir(dirpath):
+            continue 
+        if "minimized" in f:
+            continue
+        dir_list.append(dirpath) 
+       
+    total_file_counter = 0
+    valid_file_regex = re.compile(r'^session-(\d+)-(\d+)-(in|out)bound$') 
+    for d in dir_list:
+        listing = os.listdir(d)
+        for f in listing:
+            result = re.match(valid_file_regex,f)
+
+            if not result:
+                continue
+
+            inp_file = os.path.join(d,f)
+            if os.path.isdir(inp_file):
+                continue
+
+            try:
+                with open(inp_file,'rb') as f:
+                    inp_buf = f.read()
+                    md5hash = md5.new(inp_buf).digest() 
+                    hash_dict[inp_file] = md5hash 
+                    total_file_counter+=1
+            except Exception as e:
+                print e
+                continue
+
+    print "[1.1] %d entries found in dump_directory" % total_file_counter
+    print "[2.2] Added %d unique entries to hash_dict" % len(hash_dict) 
+    
+    if delim:
+        print "[3.3] Sorting the entries based on delimeter: %s"%repr(delim_buf)
+    elif indexes: 
+        print "[3.3] Sorting the entries bytes in file: %s"%str(indexes)
+    
+    request_count = 0
+    for filename in hash_dict:
+        with open(filename,"rb") as f:
+            inp_buf = f.read()
+
+        if not len(inp_buf):
+            continue
+
+        #if delim:
+        #    while  
+        
+        if indexes:
+            key = ""
+            for num in indexes:
+                try: 
+                    key+=inp_buf[num] 
+                except:
+                    print "Small file? %s" %(filename)
+            try:
+                _ = request_dict[key]    
+            except:
+                request_dict[key] = inp_buf
+                request_count+=1
+                # copy over to min dir.
+                file_id = "id_0x" + ''.join(["%02x"%ord(y) for y in key])
+                min_file = os.path.join(outdir,file_id)
+                with open(min_file,"wb") as f:
+                    f.write(inp_buf)
+                
+    template = ""
+    print "[4.4] Reduced down to %d unique requests"%request_count
+    try:
+        with open('replayer_template.py',"rb") as f:
+            template = f.read()
+    except:
+        print "[x.x] Could not find api_replayer.py template..."
+        sys.exit()    
+    
+    for i in range(0,100):
+        fname = 'api_replayer_%d.py'%i
+        if os.path.isfile(fname):
+            continue
+        with open(fname,"wb") as f: 
+            work_dir = outdir + "_workdir"
+            template = template.replace("inp_dir = %s","inp_dir = \"%s\""%outdir)
+            template = template.replace("work_dir = %s","work_dir = \"%s\""%work_dir)
+            f.write(template)
+            break
+        print "[>.>]; Why are there 100 api_replayers here....?"
+        sys.exit()
+    print "[^_^] There should hopfully be a %s script now, cheers." %fname
+                
+
+def usage():
+    print "<(^_^)> Decept's API script generator.\
+           \n**********************************\
+           \nPass it a decept --dumpraw directory and the delimeter used for the api reqs.\
+           \nHopefully you'll get a cool script to play with the api in return.\
+           \n(For nonprintable delims, use \\x0a\\x0d... format)\
+           \n\n%s <dumpraw_dir> '<api_delim>'\
+           \n\nAlternatively, use --index to sort by the set of bytes inside the index\
+           \nfiles located at the i'th indexes (for i in numberRange <numRange>) \
+           \n\n%s <dumpraw_dir> --index <numRange> (e.g. 1-4 or 1,3,9-15)\n"%(sys.argv[0],sys.argv[0]) 
+    sys.exit()
+
+
+# Takes a string of numbers, seperated via commas
+# or by hyphens, and generates an appropriate list of
+# numbers from it.
+# e.g. str("1,2,3-6")  => list([1,2,xrange(3,7)])
+#
+# If flattenList=True, will return a list of distinct elements
+#
+# If given an invalid number string, returns None
+def validateNumberRange(inputStr, flattenList=False):
+    retList = []
+    tmpList = filter(None,inputStr.split(','))
+
+    # Print msg if invalid chars/typo detected
+    for num in tmpList:
+        try:
+            retList.append(int(num))
+        except ValueError:
+            if '-' in num:
+                intRange = num.split('-')
+                # Invalid x-y-z
+                if len(intRange) > 2:
+                    print "Invalid range given"
+                    return None
+                try:
+                    if not flattenList:
+                        # Append iterator with bounds = intRange
+                        retList.append(xrange(int(intRange[0]),int(intRange[1])+1))
+                    else:
+                        # Append individual elements
+                        retList.extend(range(int(intRange[0]),int(intRange[1])+1))
+                except TypeError:
+                    print "Invalid range given"
+                    return None
+            else:
+                try:
+                    retList.append(float(num))
+                except:
+                    print "Invalid number given"
+                    return None
+    # All elements in the range are valid integers or integer ranges
+    if flattenList:
+        # If list is flattened, every element is an integer
+        retList = sorted(list(set(retList)))
+    return retList
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        usage()
+    
+    dumpdir = sys.argv[1]    
+    delim = ""
+    index_list = ""
+
+    try:
+        tmp = sys.argv.index("--index")
+        index_list = validateNumberRange(sys.argv[tmp+1],flattenList=True)
+    except ValueError:
+        delim = sys.argv[2]
+    except IndexError:
+        print "No index range for --index was found [;_;]"
+        sys.exit()
+
+    main(dumpdir,delim,index_list)
---
+++ scripts/replayer_template.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python
+import socket
+import sys
+import os
+
+# where we look for our initial first-run requests:
+inp_dir = %s 
+
+# after we've loaded > 1 time, everything is saved to here:
+work_dir = %s 
+
+# work_dir is overwritten if the --workdir param is given
+
+# for printing purposes only 
+ascii_threshold = .60
+ascii_flag = False
+
+request_dict = {}
+saved_dict = {}
+
+IP = ""
+PORT = ""
+TIMEOUT = .3
+
+changes_flag = False
+
+
+
+def main(): 
+    cmd_dict = {
+        "list":list_request,
+        "send":send_request,
+        "save":save_request,
+        "rename":rename_request,
+        "print":print_request, 
+        "exit":cleanup,
+        "quit":cleanup,
+        "chain":chain_request,
+        "new_workdir":new_workdir,
+        "load_dir":load_request_dir,
+        "load":load_request,
+        "print_mode":set_print_mode,
+        "sethost":sethost,
+        "help":print_help,
+        "?":print_help,
+    }
+
+    global work_dir
+    sethost(sys.argv[1],sys.argv[2])
+    
+    try:
+        ind = sys.argv.index("--workdir") 
+        work_dir = sys.argv[ind+1]
+    except:
+        pass
+    
+    try:
+        load_request_dir(work_dir)
+    except:
+        try:
+            load_request_dir(inp_dir)
+            os.mkdir(work_dir)
+        except:
+            print "[x.x] Unable to load %s or %s"%(work_dir,inp_dir)
+            sys.exit()
+            
+    if len(request_dict) == 0:
+        print "[x.x] Unable to read in any requests from %s" % inp_dir
+        sys.exit()
+    
+    print "[^_^] Loaded %d requests"%len(request_dict)
+
+    while True:
+        try:
+            inp = filter(None,raw_input("[^.^]> ").split(" "))
+        except:
+            print ""
+            continue 
+
+        if not len(inp):
+            continue
+        try:
+            cmd = inp[0]
+            args = inp[1:]
+            cmd_dict[cmd](*args)
+        except KeyError:
+            print "[x.x] Invalid command: %s" % inp
+        except KeyboardInterrupt:
+            continue
+        except TypeError:
+            print "[?.?] Wrong num of params for command %s"%cmd
+        except Exception as e:
+            print e
+    
+
+def sethost(ip,port=""):
+    global IP
+    global PORT
+    
+    # if someone does "ip:port", w/e.
+    if not port:
+        try:
+            ip,port = ip.split(":")
+        except:
+            print "[;_;] Bad ip/port"
+
+    try:
+        IP = sys.argv[1]
+        if len(IP.split(".")) != 4:
+            print "[>.>] Invalid IP given"
+            return
+        PORT = int(sys.argv[2])
+    except:
+        print "[x.x] Invalid params given to sethost!"
+        return
+       
+  
+def set_print_mode(mode):
+    if mode == "ascii":
+        ascii_flag = True
+    elif mode == "binary":
+        ascii_flag = False
+
+def list_request():
+    print "[!.!] Current Request Listing~" 
+    req_list = request_dict.keys()
+    req_list.sort()
+    for req in req_list:
+        print_request(req,truncate=True)
+
+def send_request(request_id):
+    try:
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+        sock.connect((IP,PORT))
+        sock.setttimeout(TIMEOUT)
+    except:
+        print "[x.x] Unable to connect to %s:%d"%(IP,PORT)
+        print "Consider using 'sethost' cmd to fix." 
+        return
+
+    
+    req = request_dict[request_id]
+    print "[>.>] Sending %d bytes~"%len(req)
+    sock.send(req)
+    
+    tmp = ""
+    ret += tmp
+    while True:
+        try:
+            tmp = sock.recv(65535)        
+            if tmp:
+                ret+=tmp
+        except:
+            break
+
+    if len(ret):
+        print "[<.<] Got %d bytes~"
+        if len(ret) > 400: 
+            ret = ret[0:400]
+        print "\\x" + "\\x".join(["%02x"%ord(y) for y in ret])
+        print "[...]"
+            
+
+def rename_request(old_request_id,new_request_id):
+    global changes_flag
+    try:
+        val = request_dict[old_request_id]
+        del request_dict[old_request_id]
+        filename = os.path.join(work_dir,old_request_id)
+        os.remove(filename)
+        changes_flag = True
+    except:
+        print "Could not remove old request %s"%old_rquest_id
+        return
+
+    save_request(new_request_id,val)
+
+
+# Will generate a new request file with name <request_id> 
+# in the work_dir that contains the request in <request_value>
+def save_request(request_id,request_value):
+    global request_dict
+    global changes_flag
+
+    try:
+        request_dict[request_id] = request_value
+    except Exception as e:
+        print "[x.x] Could not add request %s to request_dict, returning."%request_id
+        return
+
+    req_path = os.path.join(work_dir,request_id)
+
+    try:
+        with open(req_path,"wb") as f:
+            f.write(request_value)
+        changes_flag = True
+    except Exception as e:
+        print "[x.x] Could not create %s in work_dir"%request_id
+        print e
+        return 
+
+
+# Print out the given <request_id>
+def print_request(request_id,truncate=False):
+    try:
+        req = request_dict[request_id]
+    except KeyError:
+        print "[x.x] Request %s not found in request_dict"%request_id
+    
+    if truncate:
+        old_len = len(req)
+        req = req[0:200]
+        
+    buf = ""
+    for char in req:
+        if ord(char) >= 0x30 and ord(char) <= 122 and ascii_flag:
+            buf+=char
+        else:
+            buf+="\\x%02x"%ord(char)
+    print "-------------------"
+    print "%s %s" % (request_id,buf)          
+    if truncate:
+        print "[...] (200/%d bytes)" % old_len
+
+
+def chain_request():
+    print "[^_^] Not implimented, lol."
+
+def cleanup(): 
+    if changes_flag:
+        print "[?.?] Would you like to save request changes to the current workdir? (y/n)"
+        if raw_input(":").lower() == "y":
+            new_workdir(work_dir,force=True)
+    sys.exit()
+             
+
+def new_workdir(directory,force=False):
+    try:
+        os.mkdir(directory)
+    except:
+        if not force:
+            print "[?.?] Dst dir already exists, would you like to overwrite? (y/n)" 
+            if raw_input(":").lower() != "y":
+                print "[-.-] Declining to write then, returning"
+                return
+
+    for req in request_dict:
+        try:
+            req_name = os.path.join(directory,req)
+            with open(req_name,"wb") as f:
+                f.write(request_dict[req]) 
+        except Exception as e:
+            print "[;_;] Unable to save request %s to %s (%e)"%(req,directory,e)
+            if not force:
+                return
+
+def load_request_dir(directory):
+    for f in os.listdir(directory):
+        load_request(os.path.join(directory,f)) 
+
+def load_request(request_file):
+    global request_dict
+    try:
+        request_name = os.path.basename(request_file)
+        with open(request_file,"rb") as f:
+            request_dict[request_name] = f.read()
+    except Exception as e:
+        print "[x.x] Unable to load request %s (%s)" % (request_name,e)
+
+def usage():
+    print "[?.?] Usage: %s <ip> <port>" 
+    sys.exit()
+
+def print_help():
+    ret = '''\
+    <(^_^)> Decept Autogen'ed API replayer thing:
+    "list":list_request()                   - Prints out all available API requests.
+    "send":send_request(request_id)         - Sends the api request. Will cause a socket connect
+    "save":save_request(request_id,request) - Adds request to the request_dict 
+                                              and also writes a file to the workdir.
+    "rename":rename_request(old,new)        - Moves request in request_dict and filesystem. 
+    "print":print_request(request_id)       - Prints the given request for <request_id>
+    "exit":cleanup()                        - Obv.
+    "chain":chain_request(request_id1,
+                          request_id2,...)  - ??? Not sure how I want this done yet.
+    "new_workdir":new_workdir(directory)    - Writes all request entries to <directory> and 
+                                              switches work_dir to <directory>
+    "load_dir":load_request_dir(directory)  - Loads all requests from <directory> into the
+                                              current request_dict.
+    "load":load_request(file)               - Loads a single request into the request_dict
+    "print_mode":set_print_mode(mode)       - Controls how the print/list commands operate.
+                                              Available modes: ("binary"||"ascii") 
+    "sethost":sethost(ip,port)              - Change remote endpoint to <ip>:<port>
+    '''
+
+    print ret
+       
+
+if __name__ == "__main__":
+
+    print "<(^_^)> Decept Autogen'ed API replayer thing:"
+    if len(sys.argv) < 3:
+        usage() 
+    main()
